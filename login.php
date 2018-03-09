3a1868b44128321643bcab8e296cc3ceb833dc22
emmafangliao <emmafangliao@gmail.com>
2018-03-08 19:22:54 -0800

    k

--- login.php
+++ login.php
@@ -1,1 +1,41 @@
 <?php
+	
+	if(isset($_POST["submit"])) {
+		echo 'sth';
+		global $link;
+   		include("dbconnect.php");
+    	$link = new mysqli($server,$user,$password,$dbname);
+    	if ($link->connect_errno) {
+    		die("Connection failed: " . $link->connect_error);
+		} else {
+			echo"Connection successful.";
+		}
+		
+		$uname = $_POST["uname"];
+		echo $uname. "entered by user at log in <br>";
+		
+		
+		$psw = $_POST['psw'];
+		echo $psw. "password entered by user at log in  <br>";
+		
+		$q = "SELECT username, password FROM user_information WHERE username = \"$uname\"";
+		$result = $link->query($q);
+		if ($result->num_rows > 0) {
+			while($row = $result->fetch_assoc()) {	
+				echo $row["password"]. "taken from database";
+				if(password_verify($psw, $row["password"])){
+					echo 'successfully logged in';
+				} else {
+					echo 'incorrect password';
+				}
+			}
+		} 	
+				
+		
+		echo '<a href="arch482-capstone.html"> Login to see your profile </a>';
+	} else {
+		echo 'not working';
+	}
+?>
+				
+	
--- signup.php
+++ signup.php
@@ -1,3 +1,33 @@
 <?php 
+	
 	if(isset($_POST["submit"])) {
+	
+		global $link;
+   		include("dbconnect.php");
+    	$link = new mysqli($server,$user,$password,$dbname);
+    	if ($link->connect_errno) {
+    		die("Connection failed: " . $link->connect_error);
+		} else {
+			echo"Connection successful.";
+		}
+		
+		$uname = $_POST["new-uname"];
+		$email = $_POST['new-email'];
+		$psw = password_hash($_POST['psw'], PASSWORD_BCRYPT);
+		
+		
+		$q = "INSERT INTO user_information(username, password, email) 
+				VALUES (\"$uname\", \"$psw\", \"$email\")";
+				
+		if ($link->query($q) === TRUE) {
+    		echo "New account created successfully";
+		} else {
+    		echo "Error: " . $q . "<br>" . $link->error;
+		}
+		echo '<a href="arch482-capstone.html"> Login to see your profile </a>';
+	} else {
+		echo 'not working';
+	}
+?>
+				
 		
